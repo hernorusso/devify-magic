1c4e51e2dff615f23628a24949755c99a22bb778
herno <herno.russo@gmail.com>
2024-07-15 18:10:15 -0300

    Create find all method for houses
    Include a local array to mimic database data.
    It will be replaced when connection to the real database

--- src/houses/houses.service.ts
+++ src/houses/houses.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class HousesService {}
+export class HousesService {
+  // TODO: Just incremental development, this should be replaced by DB calls
+  private houses = [
+    {
+      name: 'houseName',
+      motto: 'houseMotto',
+      headOfHouse: 'houseHead',
+    },
+  ];
+
+  findAll() {
+    return this.houses;
+  }
+}
