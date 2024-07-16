d1e87d55c5b73c6d230a6cb77afaa7feccba4f78
herno <herno.russo@gmail.com>
2024-07-16 00:14:35 -0300

    Config typeORM data source

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { HousesModule } from './houses/houses.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [HousesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'devify-magic',
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    HousesModule,
+  ],
 })
 export class AppModule {}
