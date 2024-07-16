dfbdc11d5c56ee4f4eb02aa402d52bce0a52de7b
herno <herno.russo@gmail.com>
2024-07-15 21:40:50 -0300

    Implement Get houses endpoint

--- src/houses/houses.controller.ts
+++ src/houses/houses.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { HousesService } from './houses.service';
 
 @Controller('houses')
 export class HousesController {
   constructor(private readonly housesService: HousesService) {}
+
+  @Get()
+  getHouses() {
+    return this.housesService.findAll();
+  }
 }
