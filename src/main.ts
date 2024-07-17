33ebaa8103366d67aeaab3305e55baa1466067cc
herno <herno.russo@gmail.com>
2024-07-17 16:20:05 -0300

    Add Swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const docConfig = new DocumentBuilder()
+    .setTitle('Hogwarts Houses')
+    .setDescription('API to manage students allocation in Hogwarts')
+    .setVersion('1.0')
+    .addTag('houses')
+    .addTag('students')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, docConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
