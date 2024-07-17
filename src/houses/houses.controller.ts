3c3839920cc13a316790ba193031757780792868
herno <herno.russo@gmail.com>
2024-07-17 16:20:35 -0300

    Document get houses endpoint

--- src/houses/houses.controller.ts
+++ src/houses/houses.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { HousesService } from './houses.service';
 import { House } from './house.entity';
-
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+@ApiTags('houses')
 @Controller('houses')
 export class HousesController {
   constructor(private readonly housesService: HousesService) {}
 
+  @ApiResponse({
+    status: 200,
+    description: 'Return a collection of hogwarts houses',
+  })
   @Get()
   getHouses(): Promise<House[]> {
     return this.housesService.find();
