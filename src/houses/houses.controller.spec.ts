99fc615b44ef122d07fbf1e75ec71d2f97c9dd31
herno <herno.russo@gmail.com>
2024-07-17 15:30:52 -0300

    Fix controller test
    Mocking the service layer

--- src/houses/houses.controller.spec.ts
+++ src/houses/houses.controller.spec.ts
@@ -7,10 +7,16 @@ describe('HousesController', () => {
   let controller: HousesController;
   let service: HousesService;
 
+  const houseServiceMockFactory = () => ({
+    find: jest.fn(),
+  });
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HousesController],
-      providers: [HousesService],
+      providers: [
+        { provide: HousesService, useFactory: houseServiceMockFactory },
+      ],
     }).compile();
 
     controller = module.get<HousesController>(HousesController);
@@ -21,14 +27,13 @@ describe('HousesController', () => {
     expect(controller).toBeDefined();
   });
 
-  it('should get houses', () => {
-    const findAllMock = () => {
-      return [houseMock];
-    };
-    jest
-      .spyOn(service, 'findAll')
-      .mockReturnValue(Promise.resolve([houseMock]));
-    const houses = controller.getHouses();
+  it('should get houses', async () => {
+    const mockedResult = [houseMock];
+    jest.spyOn(service, 'find').mockResolvedValue(mockedResult);
+
+    const houses = await controller.getHouses();
+
+    expect(service.find).toHaveBeenCalled();
     expect(houses).toStrictEqual([houseMock]);
   });
 });
