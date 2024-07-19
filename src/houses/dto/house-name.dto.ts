3176f6460397876143ef5608ac8400c1ad6cacf1
herno <herno.russo@gmail.com>
2024-07-19 13:07:21 -0300

    Add parameter house name to  Swager doc

--- src/houses/dto/house-name.dto.ts
+++ src/houses/dto/house-name.dto.ts
@@ -1,6 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class HouseNameDto {
+  // TODO: update the house name
+  @ApiProperty({ example: 'Griff', description: 'Name of the house' })
   @IsString()
   name: string;
 }
