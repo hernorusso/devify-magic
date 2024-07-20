a65a0f7f8e104f4db5d0e375cadd212dbb443d9c
herno <herno.russo@gmail.com>
2024-07-20 13:49:23 -0300

    Inject Repository
    So later we can consume data from DB

--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,9 +1,14 @@
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
-
+import { Student } from './entities/student.entity';
 @Injectable()
 export class StudentsService {
+  constructor(
+    @InjectRepository(Student) private studentRepository: Repository<Student>,
+  ) {}
   create(createStudentDto: CreateStudentDto) {
     return 'This action adds a new student';
   }
