78cfcf5aa2b7167af8a3029d11fbd96d904497bf
herno <herno.russo@gmail.com>
2024-07-20 16:19:41 -0300

    TDD: Add get all students failing test
    Also fix jest root config for modules loader

---
+++ src/students/student-mock.ts
@@ -0,0 +1,11 @@
+const houseNameMock = 'Ravenclaw';
+
+export const studentMock = {
+  name: 'student',
+  age: 14,
+  bravery: 3,
+  loyalty: 3,
+  intelligence: 3,
+  ambition: 3,
+  house: houseNameMock,
+};
--- src/students/students.service.spec.ts
+++ src/students/students.service.spec.ts
@@ -1,13 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StudentsService } from './students.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Student } from './entities/student.entity';
+import { studentMock } from './student-mock';
 
 describe('StudentsService', () => {
   let service: StudentsService;
+  let repositoryMock;
+
+  const repositoryMockFactory = () => ({
+    find: jest.fn(),
+  });
 
   beforeEach(async () => {
-    const repositoryMockFactory = () => ({});
+    repositoryMock = repositoryMockFactory();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StudentsService],
+      providers: [
+        StudentsService,
+        { provide: getRepositoryToken(Student), useValue: repositoryMock },
+      ],
     }).compile();
 
     service = module.get<StudentsService>(StudentsService);
@@ -20,6 +32,23 @@ describe('StudentsService', () => {
   describe('Should get all students', () => {
     it('should be defined', () => {
       expect(service.findAll).toBeDefined();
+    });
+
+    it('should call the repository layer', () => {
+      jest.spyOn(repositoryMock, 'find');
+
+      service.findAll();
+
+      expect(repositoryMock.find).toHaveBeenCalled();
+    });
+
+    it('should return a collection of students', async () => {
+      const resultMock = [studentMock];
+      jest.spyOn(repositoryMock, 'find').mockResolvedValue(resultMock);
+
+      const result = await service.findAll();
+
+      expect(result).toEqual(resultMock);
     });
   });
 });
