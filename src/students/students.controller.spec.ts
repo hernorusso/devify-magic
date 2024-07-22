45a7dbf506c94647da709c903f1da267b464a8da
herno <herno.russo@gmail.com>
2024-07-22 16:06:55 -0300

    TDD: Failing constroller test: get students

--- src/students/students.controller.spec.ts
+++ src/students/students.controller.spec.ts
@@ -1,14 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
+import { studentMock } from './student-mock';
 
 describe('StudentsController', () => {
   let controller: StudentsController;
+  let serviceMock;
+  const serviceMockFactory = () => ({
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    remove: jest.fn(),
+    update: jest.fn(),
+  });
 
   beforeEach(async () => {
+    serviceMock = serviceMockFactory();
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StudentsController],
-      providers: [StudentsService],
+      providers: [{ provide: StudentsService, useValue: serviceMock }],
     }).compile();
 
     controller = module.get<StudentsController>(StudentsController);
@@ -16,5 +26,27 @@ describe('StudentsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('Get all students', () => {
+    it('should be defined', () => {
+      expect(controller.findAll).toBeDefined();
+    });
+
+    it('should call the findAll service method', async () => {
+      jest.spyOn(serviceMock, 'findAll').mockResolvedValue([studentMock]);
+
+      await controller.findAll();
+
+      expect(serviceMock.findAll).toHaveBeenCalled();
+    });
+
+    it('should return a list of students', async () => {
+      jest.spyOn(serviceMock, 'findAll').mockResolvedValue([studentMock]);
+
+      const result = await controller.findAll();
+
+      expect(result).toEqual([studentMock]);
+    });
   });
 });
