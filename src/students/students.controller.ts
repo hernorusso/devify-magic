106db4e69d76dbc0ddecd4dfef7acbf9adabfb56
herno <herno.russo@gmail.com>
2024-07-22 16:23:07 -0300

    Implement get all students controller method

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,34 +1,50 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { StudentsService } from './students.service';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Student } from './entities/student.entity';
 
+@ApiTags('students')
 @Controller('students')
 export class StudentsController {
   constructor(private readonly studentsService: StudentsService) {}
 
   @Post()
   create(@Body() createStudentDto: CreateStudentDto) {
+    console.log(createStudentDto);
     return this.studentsService.create(createStudentDto);
   }
 
+  @ApiResponse({
+    status: 200,
+    description: 'Return a collection of hogwarts students',
+  })
   @Get()
-  findAll() {
+  findAll(): Promise<Student[]> {
     return this.studentsService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.studentsService.findOne(+id);
+    return this.studentsService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateStudentDto: UpdateStudentDto) {
-    return this.studentsService.update(+id, updateStudentDto);
+    return this.studentsService.update(id, updateStudentDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.studentsService.remove(+id);
+    return this.studentsService.remove(id);
   }
 }
